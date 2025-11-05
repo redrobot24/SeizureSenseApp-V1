86519082e6bf958f830cda1ec40d4c4343c6519d
marenmccrossan <maren.mccrossan@gmail.com>
2025-11-05 11:47:34 -0500

    Maren Update

--- SeizureAppFile V1/SeizureAppFile V1/Maren.swift
+++ SeizureAppFile V1/SeizureAppFile V1/Maren.swift
@@ -5,3 +5,20 @@
 //  Created by Maren McCrossan on 11/5/25.
 //
 
+import SwiftUI
+
+struct MarenView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MarenView()
+}
