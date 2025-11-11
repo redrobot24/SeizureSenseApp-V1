6b6c332ae191b0b201e73bba8b208321964c84a8
bekahmuldoon <bekahmuldoon@gmail.com>
2025-11-11 18:32:42 -0500

    watch app

---
+++ SeizureAppFile V1/HealthData Watch App/HealthDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthDataApp.swift
+//  HealthData Watch App
+//
+//  Created by Bekah Muldoon on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthData_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
